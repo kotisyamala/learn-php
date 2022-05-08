e18c10f25555e1e170b5069215f59d109a180414
Koti <kotireddy299@gmail.com>
2022-05-08 08:19:43 -0400

    Welcome to php

---
+++ code/hello.php
@@ -0,0 +1,28 @@
+<?php
+//Basic variables
+$message = "Hello Koti! Welcome to PHP!";
+$count= 25;
+$price= 2.55;
+//echo $message;
+var_dump($message);
+var_dump($count);
+var_dump($price);
+
+//Boolean & null
+$is_admin = true;
+$data= null;
+
+var_dump($is_admin,$data);
+
+//Concadination
+$name ="koti";
+$age = 21;
+$status = true;
+$new_age = $age+10;
+$sentence = $name . " knows PHP!";
+
+var_dump($name,$age,$status,$new_age,$sentence);
+
+//Operators
+$first_name ="Koti";
+$last_name = "Syamala";
